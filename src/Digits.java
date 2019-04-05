1f107e38c58ccfd0463b27aeb81995096e816b20
BT_1N3_28 <jjacobs8462@bths.edu>
2019-04-05 08:29:56 -0400

    finished digit stuff

--- src/Digits.java
+++ src/Digits.java
@@ -4,17 +4,29 @@ import java.lang.*;
 public class Digits
 {
     //The list of digits from the number used to //construct this object
-    ArrayList<Integer> digitList;
+    ArrayList<Integer> digitList = new ArrayList<>();
 
     //Constructs a Digits object that
     //represents num
     public Digits (int num)
     {
-        //implemented in part (a)
+
+        String word = Integer.toString(num);
+        for(int i = 0; i < word.length(); i++)
+        {
+            digitList.add(Integer.parseInt(word.substring(i, i + 1)));
+        }
     }
     public boolean isStrictlyIncreasing()
     {
-        //implemented in part (b)
+        for(int i = 0; i < digitList.size(); i++)
+        {
+            if(digitList.get(i) > digitList.get(i + 1))
+            {
+                return false;
+            }
+        }
+        return true;
     }
     //Extra method to make the runner easier to read
     public ArrayList<Integer> getDigitList()
